1f97695383f67853e5d5e8653a8d4b83357b9f51
wangfaxi <wangfaxi@hawkcera.com>
2017-08-21 13:13:49 +0800

    修正编译问题

--- client/src/framework/component/HttpComponent.ts
+++ client/src/framework/component/HttpComponent.ts
@@ -1,3 +1,5 @@
+import React = require('react');
+
 import RX = require('reactxp');
 import { ComponentBase } from 'resub';
 import { HttpResponse } from "./HttpStore";
@@ -5,16 +7,22 @@ import { HttpParams } from "../utils/RestUtils";
 import BaseJson from '../models/BaseJson';
 import { HttpStore } from "./HttpStore";
 
-export type HttpProps<T> = {
+
+interface HttpProps<T> extends RX.CommonStyledProps<RX.Types.ViewStyle>{
     httpParams?: HttpParams;
-    httpStore?: HttpStore<T>; // 可能其它组件发起http ,此组件来监听结果
+    httpStore?: HttpStore<any>; // 可能其它组件发起http ,此组件来监听结果
     onIdle?: () => JSX.Element;
     onLoading?: () => JSX.Element;
-    onSucess: (result: BaseJson<T>) => JSX.Element;
+    onSucess: (result: BaseJson<any>) => JSX.Element;
     onFail?: (err: any) => JSX.Element;
 }
+interface MainPanelProps<T> {
+    httpParams?:HttpParams;
+    httpStore?:HttpStore<T>;
+    onIdle?:()=>JSX.Element;
+}
 
-export class HttpComponent<T> extends ComponentBase<HttpProps<T>, HttpResponse<T>> {
+export class HttpComponent<T> extends ComponentBase<HttpProps<T>,HttpResponse<T>> {
     protected _buildState(props: HttpProps<T>, initialBuild: boolean): HttpResponse<T> {
         let mHttpStore: HttpStore<T> = this.props.httpStore;
         if (!mHttpStore) {
@@ -24,7 +32,7 @@ export class HttpComponent<T> extends ComponentBase<HttpProps<T>, HttpResponse<T
         return mHttpStore.getHttpResonse()
     }
 
-    render(){
+    render() {
         switch (this.state.state) {
             case 'idle':
                 return this.props.onIdle() || this.props.onLoading();

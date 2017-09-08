7ce5c43b6d02ae5164d0d813e41627b934e3fa62
wangfaxi <wangfaxi@hawkcera.com>
2017-09-08 18:00:46 +0800

    增加返回处理

---
+++ client/src/ui/main/DrawerComp.tsx
@@ -0,0 +1,70 @@
+import rx = require('reactxp');
+import ReactNative = require('react-native');
+
+import fm = require('../../framework');
+
+import { Home } from './home/Home';
+import { MenuComp } from './MenuComp';
+
+export class DrawerComp extends rx.Component<{}, null>{
+
+    private _drawer: fm.component.widget.DrawerLayout;
+    private _backPressedTime: number = 0;
+
+    public componentDidMount() {
+        if (fm.utils.DeviceUtils.isAndroid) {
+            ReactNative.BackAndroid.addEventListener('hardwareBackPress', this.handleBackButton);
+        }
+    }
+
+    public componentWillUnmount() {
+        if (fm.utils.DeviceUtils.isAndroid) {
+            ReactNative.BackAndroid.removeEventListener('hardwareBackPress', this.handleBackButton);
+        }
+    }
+    private handleBackButton = () => {
+        if (this._isDrawerOpen()) {
+            this._toggleDrawer();
+            return true;
+        }
+        let curTime = new Date().getTime();
+        if (curTime - this._backPressedTime < 2 * 1000) {
+            // ReactNative.BackAndroid.exitApp();
+            return false;
+        } else {
+            this._backPressedTime = curTime;
+            fm.utils.ToastUtils.showToast('press back again');
+            return true;
+        }
+    }
+    render() {
+        return (
+            <fm.component.widget.DrawerLayout
+                ref={(ref: fm.component.widget.DrawerLayout) => this._drawer = ref}
+                drawerWidth={270}
+                drawerPosition='right'
+                renderNavigationView={this._renderDrawer}
+            >
+                <Home isDrawerOpen={this._isDrawerOpen} toggleDrawer={this._toggleDrawer} />
+            </fm.component.widget.DrawerLayout>
+        );
+    }
+    _renderContent = (): JSX.Element => {
+        return (
+            <Home isDrawerOpen={this._isDrawerOpen} toggleDrawer={this._toggleDrawer} />
+        );
+    }
+    _isDrawerOpen = () => {
+        return this._drawer.state.drawerShown;
+    }
+    _toggleDrawer = () => {
+        if (this._drawer.state.drawerShown) {
+            this._drawer.closeDrawer();
+        } else {
+            this._drawer.openDrawer();
+        }
+    }
+    _renderDrawer = () => {
+        return (<MenuComp />);
+    }
+}
